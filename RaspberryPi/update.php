821f05c83863d4a7774493517aa2da0b4da69c90
Jonathan Schooler <violinplaya@gmail.com>
2014-08-26 22:22:26 -0700

    PHP service supports batching via JSON POST requests

--- RaspberryPi/update.php
+++ RaspberryPi/update.php
@@ -1,53 +1,77 @@
 <?php
-try {
 
-	if (!isset($_GET['loc']))
-		throw new Exception('no get data');
-	
+// define the query string first
+$query = " 
+	INSERT INTO locations (
+		mac,
+		rssi,
+		room,
+		time
+	) VALUES ( 
+		:mac, 
+		:rssi,
+		:room, 
+		:time
+	) 
+";
 
-	
-	$a = explode(',',$_GET['loc']);
-	$mac = (string) $a[0];
-	$rssi = (float) $a[1];
-	$room = (float) $a[2];
-	$time = time();
-	
-	print "Got: mac = ".$mac." and rssi = ".$rssi . "<br/>";
-	
-	$db = new PDO("sqlite:./db/data.db");
-	
- $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);	
-	
-	$query = " 
-		INSERT INTO locations (
-			mac,
-			rssi,
-			room,
-			time
-		) VALUES ( 
-			:mac, 
-			:rssi,
-			:room, 
-			:time
-		) 
-	";
-	
+/*
+The POST body has this JSON format:
+[
+	{
+		"mac": "48:2C:6A:1E:59:3D",
+		"rssi": -45,
+		"room": 3,
+		"time": 1409108787
+	},
+	{
+		"mac": "48:2C:6A:1E:59:3D",
+		"rssi": -44,
+		"room": 3,
+		"time": 1409108788
+	},
+	...
+	{
+		"mac": "48:2C:6A:1E:59:3D",
+		"rssi": -55,
+		"room": 3,
+		"time": 1409108804
+	}
+]
+*/
+function insert_data_point($data_point) {
+	$mac = strtolower($data_point['mac']);
+	$rssi = $data_point['rssi'];
+	$room = $data_point['room'];
+	$time = $data_point['time'];
+
 	$stmt = $db->prepare($query);
 	$stmt->bindParam(':mac', $mac, SQLITE3_TEXT);
 	$stmt->bindParam(':rssi', $rssi, SQLITE3_INTEGER);
 	$stmt->bindParam(':room', $room, SQLITE3_INTEGER);
 	$stmt->bindParam(':time', $time, SQLITE3_INTEGER);
 	$stmt->execute();
-	
-	print '<br>';
-	
-	print 'all done <br>';
-	
-	
-	
 }
-catch(Exception $e) {
-	print "<br/>";
-	print $e;
+
+// set up the database
+$db = new PDO("sqlite:./db/data.db");
+$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+// read the POST body
+$request_body = file_get_contents('php://input');
+
+// decode the JSON
+$data_points = json_decode($request_body, true);
+
+// loop over the elements of the array, inserting each one into the db
+foreach ($data_points as $data_point) {
+	insert_data_point($data_point);
 }
+
+// respond with 204 No Content if everything goes well
+http_response_code(204);
+
+// TODO: add gzip compression support
+// TODO: send http_response_code(400) for bad requests
+
 ?>
