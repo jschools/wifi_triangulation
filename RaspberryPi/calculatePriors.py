d082693463e27a4709553f2c8dd9e4999b51f6fc
Zack <zack.scholl@gmail.com>
2014-08-30 14:40:55 -0400

    Update calculatePriors.py
    Fixed nP

--- RaspberryPi/calculatePriors.py
+++ RaspberryPi/calculatePriors.py
@@ -23,9 +23,9 @@ for mac in macs:
 	Wdefault[mac]=0
 	P[mac]={}
 	nP[mac]={}
+	nP[mac] = numpy.zeros(100)
 	for room in rooms:
 		P[mac][room] = numpy.zeros(100)
-		nP[mac][room] = numpy.zeros(100)
 
 # Add a Gaussian centered around RSSI with STD of 1 RSSI - this takes awhile
 for mac in macs:
@@ -41,17 +41,17 @@ for mac in macs:
 				P[mac][room][row[0]+103]=P[mac][room][row[0]+103]+0.06
 			except Exception,e:
 				print str(e)
-		for row in c.execute("select rssi from locations where mac like '" + mac + "' and room>0"):
-			try:
-				nP[mac][room][row[0]+100]=nP[mac][room][row[0]+100]+0.4
-				nP[mac][room][row[0]+99]=nP[mac][room][row[0]+99]+0.14
-				nP[mac][room][row[0]+101]=nP[mac][room][row[0]+101]+0.14
-				nP[mac][room][row[0]+98]=nP[mac][room][row[0]+98]+0.06
-				nP[mac][room][row[0]+102]=nP[mac][room][row[0]+102]+0.06
-				nP[mac][room][row[0]+97]=nP[mac][room][row[0]+97]+0.06
-				nP[mac][room][row[0]+103]=nP[mac][room][row[0]+103]+0.06
-			except Exception,e:
-				print str(e)
+	for row in c.execute("select rssi from locations where mac like '" + mac + "' and room>0"):
+		try:
+			nP[mac][row[0]+100]=nP[mac][row[0]+100]+0.4
+			nP[mac][row[0]+99]=nP[mac][row[0]+99]+0.14
+			nP[mac][row[0]+101]=nP[mac][row[0]+101]+0.14
+			nP[mac][row[0]+98]=nP[mac][row[0]+98]+0.06
+			nP[mac][row[0]+102]=nP[mac][row[0]+102]+0.06
+			nP[mac][row[0]+97]=nP[mac][row[0]+97]+0.06
+			nP[mac][row[0]+103]=nP[mac][row[0]+103]+0.06
+		except Exception,e:
+			print str(e)
 
 # close the database
 conn.close()
@@ -60,13 +60,13 @@ conn.close()
 for mac in macs:
 	for room in rooms:
 		pTotal = sum(P[mac][room])
-		npTotal = sum(nP[mac][room])
 		if pTotal>0:
 			for i in range(100):
 				P[mac][room][i] = P[mac][room][i]/pTotal
-		if npTotal>0:
-			for i in range(100):
-				nP[mac][room][i] = nP[mac][room][i]/npTotal
+	npTotal = sum(nP[mac])
+	if npTotal>0:
+		for i in range(100):
+			nP[mac][i] = nP[mac][i]/npTotal
 
 # Dumpe them to a pickle
 data = pickle.dumps(P,2)
